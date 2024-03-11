6928795f2f2f050b8bdcd31cf363174ba2e15d25
lps08 <lucianolps08@gmail.com>
2024-03-11 16:28:11 -0300

    Added method to retrieve pdf creation date and raise for status check

--- src/scraping/models/pdf_model.py
+++ src/scraping/models/pdf_model.py
@@ -1,7 +1,8 @@
 class PDFLinkModel:
-    def __init__(self, host, name, date) -> None:
+    def __init__(self, host, name, created, date) -> None:
         self.host = host
         self.name = name
+        self.created = created
         self.date = date
 
     def __str__(self) -> str:
