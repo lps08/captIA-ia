4ce3b1eb4617a7c6450d4cf3db1d16ca8c7e7ebe
lps08 <lucianolps08@gmail.com>
2024-05-04 11:22:08 -0300

    Created parse to remove unknown char, doubles ";" and remove extra spaces

--- main_pdf_extraction.py
+++ main_pdf_extraction.py
@@ -71,6 +71,90 @@ def get_pdf_infos(pdf_path, model_to_use:ModelCard = constants.MODEL_TO_USE):
         )
         return infos
 
+def remove_unknown_characters(text, unknown_characters_dict = {"\x00": "", "�": "ti"}):
+    """
+    Removes unknown characters from text.
+
+    This function takes a text string and removes any unknown characters based on the specified dictionary of unknown characters and their replacements.
+
+    Args:
+        text (str): The text string containing unknown characters.
+        unknown_characters_dict (dict): A dictionary containing unknown characters as keys and their corresponding replacements as values.
+
+    Returns:
+        str: The text string with unknown characters removed.
+
+    Notes:
+        This function iterates through the dictionary of unknown characters and their replacements, replacing each occurrence of an unknown character in the input text with its corresponding replacement.
+
+    Example:
+        To remove unknown characters from a text string, you can use this function as follows:
+
+        >>> text = "This is a test string with unknown characters � and \x00."
+        >>> cleaned_text = remove_unknown_characters(text)
+        >>> print(cleaned_text)
+        'This is a test string with unknown characters ti and .'
+    """
+    for unk_char in unknown_characters_dict:
+        text = text.replace(unk_char, unknown_characters_dict[unk_char])
+    return text
+
+def remove_a_lot_of_spaces_from_text(text):
+    """
+    Removes excessive spaces and newline characters from text.
+
+    This function removes excessive spaces and newline characters from the input text using regular expressions.
+
+    Args:
+        text (str): The input text containing excessive spaces and newline characters.
+
+    Returns:
+        str: The text with excessive spaces and newline characters removed.
+
+    Notes:
+        This function uses regular expressions to replace consecutive whitespace characters (including spaces and newline characters) with a single space character.
+
+    Example:
+        To remove excessive spaces and newline characters from a text, you can use this function as follows:
+
+        >>> text = "This   is     a    text    with\n excessive     spaces     and\n\nnewline characters."
+        >>> cleaned_text = remove_a_lot_of_spaces_from_text(text)
+        >>> print(cleaned_text)
+        'This is a text with excessive spaces and newline characters.'
+    """
+    text = re.sub(r'\n+', ' ', text)
+    text = re.sub(r'\s+', ' ', text)
+    return text.strip()
+
+def parse_list(text_list):
+    """
+    Parses a list of text items into a single string.
+
+    This function takes a list of text items and parses them into a single string by joining them with a semicolon (;) delimiter. It also removes excessive spaces and newline characters from the resulting string.
+
+    Args:
+        text_list (list or str): The list of text items to be parsed. If a string is provided, it is assumed to be a semicolon-separated list.
+
+    Returns:
+        str: The parsed text string.
+
+    Notes:
+        This function first joins the text items in the list using a semicolon (;) delimiter. It then removes excessive spaces and newline characters from the resulting string using the remove_a_lot_of_spaces_from_text function.
+
+    Example:
+        To parse a list of text items into a single string, you can use this function as follows:
+
+        >>> text_list = ["Item 1", " Item 2 ", "Item   3", " Item 4  "]
+        >>> parsed_text = parse_list(text_list)
+        >>> print(parsed_text)
+        'Item 1;Item 2;Item 3;Item 4'
+    """
+    text = ";".join(text_list) if type(text_list) == list else text_list
+    text = re.sub(r'(\s*;+\s*)+', ';', text)
+    text = remove_a_lot_of_spaces_from_text(text)
+    text = remove_unknown_characters(text)
+    return text
+
 def parse_areas(areas_list, max_size=3):
     """
     Parses and retrieves a subset of areas of knowledge from a list of areas.
@@ -96,7 +180,36 @@ def parse_areas(areas_list, max_size=3):
     """
     areas_list = [i.strip() for i in areas_list if i != '']
     areas_list.sort(key=len)
-    return areas_list[:max_size]
+    areas_list_top = areas_list[:max_size]
+    text_areas = parse_list(areas_list_top)
+    return text_areas
+
+def parse_elegibilidade(text):
+    """
+    Parses eligibility criteria from text.
+
+    This function takes a text string containing eligibility criteria and parses it into a formatted string. It first removes excessive spaces and newline characters from the input text using the parse_list function.
+
+    Args:
+        text (str): The text string containing eligibility criteria.
+
+    Returns:
+        str: The parsed eligibility criteria string.
+
+    Notes:
+        This function calls the parse_list function to remove excessive spaces and newline characters from the input text. The resulting string is returned as the parsed eligibility criteria.
+
+    Example:
+        To parse eligibility criteria from a text string, you can use this function as follows:
+
+        >>> text = ["Eligibility criteria 1", "Eligibility criteria 2", "Eligibility criteria 3"]
+        >>> parsed_elegibilidade = parse_elegibilidade(text)
+        >>> print(parsed_elegibilidade)
+        'Eligibility criteria 1;Eligibility criteria 2;Eligibility criteria 3'
+    """
+    text = parse_list(text)
+    text += "." if text[-1] != "." else ""
+    return text
 
 def parse_money_value(text):
     """
@@ -197,8 +310,65 @@ def parse_title(list_titles, min_lenght=18, min_words_merge=5):
             if len(new_title) <= min_lenght:
                 range_list_accepted -= 1
         title = new_title if len(new_title) >= min_lenght else title
-
+    title = remove_a_lot_of_spaces_from_text(title)
+    title = remove_unknown_characters(title)
     return title
+
+def parse_full_title(text):
+    """
+    Parses the full title text.
+
+    This function takes a full title text string and performs preprocessing by removing unknown characters and extra spaces.
+
+    Args:
+        text (str): The full title text string to be parsed.
+
+    Returns:
+        str: The parsed full title text string.
+
+    Notes:
+        This function first removes any unknown characters from the input text using the `remove_unknown_characters` function. Then, it removes extra spaces using the `remove_a_lot_of_spaces_from_text` function.
+
+    Example:
+        To parse a full title text string, you can use this function as follows:
+
+        >>> full_title_text = "This is a full title text with unknown \n\ncharacters � and extra    spaces."
+        >>> parsed_text = parse_full_title(full_title_text)
+        >>> print(parsed_text)
+        'This is a full title text with unknown characters and extra spaces.'
+    """
+    text = remove_unknown_characters(text)
+    text = remove_a_lot_of_spaces_from_text(text)
+    return text
+
+def parse_objetivo(text):
+    """
+    Parses the objective text.
+
+    This function takes an objective text string and performs preprocessing by removing unknown characters, extra spaces, and ensuring proper capitalization and punctuation.
+
+    Args:
+        text (str): The objective text string to be parsed.
+
+    Returns:
+        str: The parsed objective text string.
+
+    Notes:
+        This function first removes any unknown characters from the input text using the `remove_unknown_characters` function. Then, it removes extra spaces using the `remove_a_lot_of_spaces_from_text` function. After that, it ensures that the first letter of the text is capitalized and adds a period at the end if it's missing.
+
+    Example:
+        To parse an objective text string, you can use this function as follows:
+
+        >>> objective_text = "this is an objective text with unknown characters � and extra spaces"
+        >>> parsed_text = parse_objetivo(objective_text)
+        >>> print(parsed_text)
+        'This is an objective text with unknown characters and extra spaces.'
+    """
+    text = remove_unknown_characters(text)
+    text = remove_a_lot_of_spaces_from_text(text)
+    text = text[0].upper() + text[1:]
+    text += "." if text[-1] != "." else ""
+    return text
 
 def parse_datetime(text, defaul_return="Fluxo contínuo", language='br', date_order='DMY', day_of_month='first'):
     """
@@ -230,6 +400,41 @@ def parse_datetime(text, defaul_return="Fluxo contínuo", language='br', date_or
     date_found = search_dates(text, languages=[language], settings={'DATE_ORDER': date_order, 'PREFER_DAY_OF_MONTH': day_of_month})
     date = date_found[-1][1] if date_found else defaul_return
     return date
+
+def parse_nivel_trl(text):
+    """
+    Parses the Technology Readiness Level (TRL) from text.
+
+    This function extracts the Technology Readiness Level (TRL) information from text using regular expressions. It searches for TRL values in various formats and returns the extracted TRL level.
+
+    Args:
+        text (str): The text containing TRL information.
+
+    Returns:
+        str: The extracted TRL level, or 'Não encontrado' (Not found) if no TRL level is found.
+
+    Notes:
+        This function uses regular expressions to search for TRL values in text. It searches for TRL levels expressed as single numbers or ranges (e.g., "3", "7 ou 8", "4 a 6") and returns the extracted TRL level. If no TRL level is found in the text, it returns 'Não encontrado'.
+
+    Example:
+        To extract the Technology Readiness Level (TRL) from text, you can use this function as follows:
+
+        >>> text = "O nível de maturidade tecnológica necessário é 5 a 7."
+        >>> trl_level = parse_nivel_trl(text)
+        >>> print(trl_level)
+        '5 a 7'
+    """
+    nivel_trl_regex = re.compile(r"\b[0-9](?:\s*(?:a|e|ou)\s*[0-9])+\b", re.IGNORECASE)
+    res = nivel_trl_regex.search(text)
+    if res:
+        return res.group()
+    else:
+        single_num_regex = re.compile(r"\b[0-9]\b")
+        res = single_num_regex.search(text)
+        if res:
+            return res.group()
+        else:
+            return 'Não encontrado'
 
 def extract_pdf_infos_db(model_to_use:ModelCard = constants.MODEL_TO_USE):
     """
@@ -277,14 +482,14 @@ def extract_pdf_infos_db(model_to_use:ModelCard = constants.MODEL_TO_USE):
                         ds_link_pdf=edital['ds_link_pdf'],
                         ds_agency=edital['ds_agency'].upper(),
                         ds_titulo=parse_title(infos['titulo']),
-                        ds_titulo_completo = infos['titulo_completo'].replace('\n', ' '),
+                        ds_titulo_completo = parse_full_title(infos['titulo_completo']),
                         ds_numero=parse_edital_number(infos['numero']),
-                        ds_objetivo=infos['objetivo'],
-                        ds_elegibilidade='; '.join(infos['elegibilidade']) if type(infos['elegibilidade']) == list else infos['elegibilidade'],
+                        ds_objetivo=parse_objetivo(infos['objetivo']),
+                        ds_elegibilidade=parse_elegibilidade(infos['elegibilidade']),
                         dt_submissao=parse_datetime(infos['submissao']),
                         ds_financiamento=parse_money_value(infos['financiamento']),
-                        ds_areas='; '.join(parse_areas(infos['areas'])) if type(infos['areas']) == list else infos['areas'],
-                        ds_nivel_trl=infos['nivel_trl'],
+                        ds_areas=parse_areas(infos['areas']),
+                        ds_nivel_trl=parse_nivel_trl(infos['nivel_trl']),
                     )
 
             except Exception as e:
