6cd5e1df084179bfec9ad469419b63619c97361f
lps08 <lucianolps08@gmail.com>
2024-06-04 16:40:53 -0300

    Created 3 basemodels from edital basemodel for local llm inference

--- src/ml_models/llm/base_models/edital_model.py
+++ src/ml_models/llm/base_models/edital_model.py
@@ -12,3 +12,19 @@ class Edital(BaseModel):
     financiamento: str = Field(..., description='valor do financiamento do edital')
     areas: List[str] = Field(..., description='areas de conhecimento da chamada, podendo ser uma ou mais áreas')
     nivel_trl: None | str = Field(..., description='nivel de maturidade tecnológica (TRLs)')
+
+class EditalGeneralInfo(BaseModel):
+    titulo_completo: str = Field(..., description='título principal completo do edital/chamada')
+    titulos: List[str] = Field(..., description='Os título do edital/chamada')
+    numero: None | str = Field(..., description='numero do edital')
+    objetivo: str = Field(..., description='o objetivo/objeto completo do edital')
+    nivel_trl: None | str = Field(..., description='nivel de maturidade tecnológica (TRLs)')
+
+class EditalValuesInfo(BaseModel):
+    date_lanc: str = Field(..., description='data de lançamento do edital')
+    date_sub: str = Field(..., description='data de submissao do edital')
+    financiamento: str = Field(..., description='valor do financiamento do edital')
+
+class EditalDomainRequirements(BaseModel):
+    areas: List[str] = Field(..., description='areas de conhecimento da chamada podendo ser uma ou mais áreas', enum = ['Ciências Naturais', 'Ciências Sociais', 'Humanidades', 'Tecnologia da Informação', 'Matemática e Estatística', 'Saúde e Medicina', 'Educação', 'Negócios e Economia', 'Meio Ambiente e Sustentabilidade', 'Engenharia e Tecnologia'], min_items=1)
+    elegibilidade: List[str] = Field(..., description='criterios de elegibilidade do edital')
