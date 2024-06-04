36069047e3c04803a341ff3229dc8c81cde5af3b
lps08 <lucianolps08@gmail.com>
2024-06-04 16:56:25 -0300

    Implemented prompt method to generate prompt instructions based on model chat template

--- src/ml_models/llm/retrieval_qa_llm.py
+++ src/ml_models/llm/retrieval_qa_llm.py
@@ -3,6 +3,7 @@ from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain.embeddings import HuggingFaceEmbeddings
+from transformers import AutoTokenizer
 from src import constants
 
 def get_google_embeddings(model_name=constants.GOOGLE_EMBEDDINGS_MODEL_NAME):
@@ -93,34 +94,73 @@ def get_prompt(parser):
         }
     )
 
-def qa_llm(query, llm, retriever, parser=None, chain_type = "stuff"):
+def get_prompt_role(parser, tokenizer_name=constants.LOCAL_LLM_MODEL_NAME):
     """
-    Perform question answering using a language model and a retriever.
+    Creates a prompt template for a language model to follow specific roles and instructions.
+
+    This function defines a prompt template for an assistant role that answers questions based on provided context. The assistant should only answer what is asked and avoid making up answers. The function uses a specified tokenizer to apply a chat template to the messages.
 
     Args:
-        query (str): The question/query to be answered.
-        llm: The language model used for question answering.
-        retriever: The retriever used for retrieving relevant documents.
-        parser: The parser object used for parsing the answer. Defaults to None.
-        chain_type (str, optional): The type of retrieval QA chain to use. Defaults to "stuff".
+        parser: An object that provides format instructions for the model's responses.
+        tokenizer_name (str, optional): The name of the tokenizer to use for applying the chat template. Defaults to `constants.LOCAL_LLM_MODEL_NAME`.
 
     Returns:
-        dict or str: The result of the question answering process.
-
-    Note:
-        This function performs question answering using a language model and a retriever.
-        It constructs a retrieval QA chain based on the provided language model, retriever, and optional parser.
-        The chain is invoked with the provided query to retrieve the answer.
-        If a parser is provided, the result is parsed using the parser object.
-        The final result is returned as a string.
+        PromptTemplate: A template configured with the given messages and format instructions, ready for use with a language model.
 
     Example:
-        >>> question = "What is the capital of Brazil?"
-        >>> language_model = get_language_model()
-        >>> document_retriever = get_document_retriever()
-        >>> answer = qa_llm(question, language_model, document_retriever)
+        >>> parser = SomeParser()
+        >>> prompt_template = get_prompt_role(parser)
     """
-    prompt = get_prompt(parser) if parser else None
+    messages = [
+        {
+            "role": "system",
+            "content": "You are a assistant and answer what is asked. Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know returning 'Não encontrado', don't try to make up an answer! Answer only what is asked, please! Do not include any comments or notes in the outputed answer!",
+        },
+        {
+            "role": "user",
+            "content": 'Context: \n{context}\nBased on the information of the context, answer this question: {question}.\nFollow the requirements below:\n{format_instructions}',
+        },
+    ]
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
+    prompt_template_instructions = tokenizer.apply_chat_template(messages, add_generation_prompt=True, tokenize=False)
+
+    return PromptTemplate(
+        template=prompt_template_instructions, 
+        input_variables=['context', 'question'], 
+        partial_variables={
+            "format_instructions": parser.get_format_instructions()
+        }
+    )
+
+def qa_llm(query, llm, retriever, parser=None, use_role=False, chain_type = "stuff"):
+    """
+    Executes a question-answering task using a language model (LLM) and a retriever.
+
+    This function takes a query and utilizes a language model along with a retriever to extract relevant information from documents. It supports two types of prompts: a role-based prompt or a standard prompt, which can be customized with a parser.
+
+    Args:
+        query (str): The question or query to be answered by the language model.
+        llm: The language model to use for generating the answer.
+        retriever: The retriever used to fetch relevant documents or passages.
+        parser (optional): An object that provides format instructions for the model's responses. Defaults to None.
+        use_role (bool, optional): Whether to use a role-based prompt. Defaults to False.
+        chain_type (str, optional): The type of chain to use for the retrieval-based QA. Defaults to "stuff".
+
+    Returns:
+        str: The result of the question-answering task, extracted from the response dictionary.
+
+    Example:
+        >>> query = "What is the objective of the project described in the document?"
+        >>> llm = get_language_model()
+        >>> retriever = create_retriever(documents, embeddings)
+        >>> parser = SomeParser()
+        >>> answer = qa_llm(query, llm, retriever, parser, use_role=True)
+        >>> print(answer)
+    """
+    if use_role:
+        prompt = get_prompt_role(parser)
+    else:
+        prompt = get_prompt(parser) if parser else None
     chain = RetrievalQA.from_chain_type(llm=llm, chain_type=chain_type, retriever=retriever, chain_type_kwargs={"prompt" : prompt})
     res = chain.invoke(query)
     return res['result']
